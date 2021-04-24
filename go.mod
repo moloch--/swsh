2a191056728d18f048487f2dcf1c5e0aa85b02d4
moloch-- <875022+moloch--@users.noreply.github.com>
2021-04-24 12:43:04 -0500

    v1

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/moloch--/starshell
+
+go 1.16
+
+require github.com/buger/goterm v1.0.0
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	_ "embed"
+	"strconv"
+	"strings"
+	"time"
+
+	tm "github.com/buger/goterm"
+)
+
+//go:embed film.txt
+var film string
+
+func main() {
+	film = strings.ReplaceAll(film, "\\n", "\n")
+	frames := strings.Split(film, "\n")
+	tm.Clear()
+
+	speed := time.Millisecond * 64
+
+	for index := 0; index < len(frames); index += 14 {
+		tm.Clear()
+		tm.MoveCursor(1, 1)
+		frameControl, err := strconv.Atoi(frames[index])
+		if err != nil {
+			frameControl = 1
+		}
+		tm.Printf("%s\n", frames[index+1])
+		tm.Printf("%s\n", frames[index+2])
+		tm.Printf("%s\n", frames[index+3])
+		tm.Printf("%s\n", frames[index+4])
+		tm.Printf("%s\n", frames[index+5])
+		tm.Printf("%s\n", frames[index+6])
+		tm.Printf("%s\n", frames[index+7])
+		tm.Printf("%s\n", frames[index+8])
+		tm.Printf("%s\n", frames[index+9])
+		tm.Printf("%s\n", frames[index+10])
+		tm.Printf("%s\n", frames[index+11])
+		tm.Printf("%s\n", frames[index+12])
+		tm.Printf("%s\n", frames[index+13])
+		tm.Flush()
+
+		for index := 0; index < frameControl; index++ {
+			time.Sleep(speed)
+		}
+	}
+
+}
